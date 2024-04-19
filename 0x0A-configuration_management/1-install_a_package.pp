4769e2cd7bd8816857742830106d03cc8cd0f612
thebasitguy <mailbasitgh@gmail.com>
2024-04-19 10:07:39 -0700

    checked

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,9 @@
-# Ensure pip3 is installed
+# Ensures pip3 is installed
 package { 'python3-pip':
   ensure => installed,
 }
 
-# Install Flask
+# Installs Flask
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',

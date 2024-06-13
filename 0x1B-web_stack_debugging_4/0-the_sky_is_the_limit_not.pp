0593ed2c529fa37af962e38687c0f66c98511379
thebasitguy <mailbasitgh@gmail.com>
2024-06-13 13:40:28 +0000

    task files

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,6 @@
-# This increases the amount of traffic Nginx server can handle.
+#This increases the amount of traffic Nginx server can handle.
 
-# Increase the ULIMIT of the default file.
-exec { 'fix--for-nginx':
-	command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-	path	=> '/usr/local/bin/:/bin/',
-}
-
-# To restart Nginx
-exec { 'nginx-restart':
-	command => '/etc/init.d/nginx restart',
-	path => '/etc/init.d/',
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,6 @@
 # This enables user holberton login and open files without errors.
 
-exec { 'increasing-hard-file-limit-for-holberton-user':
-  command => "sed -i "/^holberton hard/s/5/50000/' /etc/security/limits.conf",
-  path	=> '/usr/local/bin/:/bin/'
-}
-
-exec { 'increasing-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton soft/s/4/50000/' /etc/security/limits.conf',
-  path  => '/usr/local/bin/:/bin/'
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
